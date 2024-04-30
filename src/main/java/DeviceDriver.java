f4cebee091230845e673bb6c8717c7d1716b9d01
yeonwoo.kang <kywoo26@naver.com>
2024-04-30 16:16:31 +0900

    add to read

--- src/main/java/DeviceDriver.java
+++ src/main/java/DeviceDriver.java
@@ -1,15 +1,41 @@
+import org.assertj.core.util.VisibleForTesting;
+
 /**
  * This class is used by the operating system to interact with the hardware 'FlashMemoryDevice'.
  */
 public class DeviceDriver {
+    public static final int ITERATION_FACTOR = 5;
+    private static final byte[] buffer = new byte[ITERATION_FACTOR];
 
-    public DeviceDriver(FlashMemoryDevice hardware) {
-        // TODO: implement this method
+    private final FlashMemoryDevice flashMemory;
+
+    public DeviceDriver(FlashMemoryDevice flashMemory) {
+        this.flashMemory = flashMemory;
     }
 
-    public byte read(long address) {
-        // TODO: implement this method
-        return -1;
+    public byte read(long address) throws ReadFailException {
+        readToBuffer(address);
+
+        if (!isConsistentBuffer()) {
+            throw new ReadFailException("Failed to read");
+        }
+
+        return buffer[0];
+    }
+
+    private void readToBuffer(long address) {
+        for (int i = 0; i < ITERATION_FACTOR; i++) {
+            buffer[i] = flashMemory.read(address);
+        }
+    }
+
+    private boolean isConsistentBuffer() {
+        for (int i = 1; i < ITERATION_FACTOR; i++) {
+            if (buffer[i - 1] != buffer[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public void write(long address, byte data) {
--- src/test/java/DeviceDriverTest.java
+++ src/test/java/DeviceDriverTest.java
@@ -1,14 +1,48 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class DeviceDriverTest {
 
+    private static final byte EMPTY = (byte) 0xFF;
+    private static final int BASE_ADDRESS = 0x00;
+
+    @Mock
+    private FlashMemoryDevice flashMemory;
+
+    private DeviceDriver deviceDriver;
+
+    @BeforeEach
+    void setUp() {
+        deviceDriver = new DeviceDriver(flashMemory);
+    }
+
     @Test
-    public void read_From_Hardware() {
-        // TODO: replace hardware with a Test Double
-        FlashMemoryDevice hardware = null;
-        DeviceDriver driver = new DeviceDriver(hardware);
-        byte data = driver.read(0xFF);
-        assertEquals(0, data);
+    void testReadConsistentData() {
+        doReturn(EMPTY).when(flashMemory).read(BASE_ADDRESS);
+        byte data = deviceDriver.read(BASE_ADDRESS);
+
+        verify(flashMemory, times(DeviceDriver.ITERATION_FACTOR)).read(BASE_ADDRESS);
+        assertThat(data).isEqualTo(EMPTY);
+    }
+
+    @Test
+    void testReadInconsistentData() {
+        doReturn((byte) 0x01, (byte) 0x02).when(flashMemory).read(BASE_ADDRESS);
+
+        assertThatThrownBy(() -> deviceDriver.read(BASE_ADDRESS))
+                .isInstanceOf(ReadFailException.class)
+                .hasMessage("Failed to read");
     }
 }
